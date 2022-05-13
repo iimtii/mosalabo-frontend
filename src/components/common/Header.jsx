ef87073ca27ddfee6a05aab0ca6d132b48700a7d
Tomoyuki Hiranuma <tomoyuki.hiranuma@rakuten.com>
2022-05-13 10:16:05 +0900

    fix: header logo

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { Box, Center, Flex } from "@chakra-ui/react";
 import { css } from "@emotion/react";
 import { typography } from "../../styles/common";
+import Link from "next/link";
 
 export const Header = () => {
   const style = {
@@ -12,17 +13,42 @@ export const Header = () => {
 
   return (
     <header>
-      <Center height={"100px"}>
-        <Flex gap={`20px`}>
-          <Flex alignItems={`center`} justifyContent={`center`}>
-            <Image
-              alt="logo"
-              src={`/icons/Logo.svg`}
-              width={`40px`}
-              height={`40px`}
-            />
+      <Center height={"100px"} marginTop={`40px`}>
+        <Flex gap={`100px`}>
+          <Box>
+            <Link href={"/"}>
+              <a>
+                <Flex gap={`20px`}>
+                  <Flex alignItems={`center`} justifyContent={`center`}>
+                    <Image
+                      alt="logo"
+                      src={`/icons/Logo.svg`}
+                      width={`40px`}
+                      height={`40px`}
+                    />
+                  </Flex>
+                  <Box css={style.font}>Mosalabo</Box>
+                </Flex>
+              </a>
+            </Link>
+          </Box>
+          <Flex
+            alignItems={`center`}
+            justifyContent={`center`}
+            paddingTop={`3px`}
+          >
+            <Link href={`/`}>
+              <a>
+                <Image
+                  alt="home"
+                  src={`/icons/home.svg`}
+                  width={`40px`}
+                  height={`40px`}
+                  css={style.home}
+                />
+              </a>
+            </Link>
           </Flex>
-          <Box css={style.font}>Mosalabo</Box>
         </Flex>
       </Center>
     </header>
