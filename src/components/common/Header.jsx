da42d28c6b4276431fee3c7e3349f5336555eb17
imtiar <imtiar.jahan@rakuten.com>
2022-05-23 14:19:55 +0900

    edited back modal layout

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -7,9 +7,7 @@ import {
   Modal,
   ModalHeader,
   ModalContent,
-  ModalBody,
   ModalOverlay,
-  ModalFooter,
   Button,
   Heading,
 } from "@chakra-ui/react";
@@ -74,28 +72,32 @@ export const Header = () => {
         <ModalOverlay />
         <ModalContent>
           <ModalHeader>
-            <Heading size={`lg`}>本当に移動しますか？</Heading>
-            <Heading size={`sm`} opacity={`0.5`}>
+            <Heading size={`lg`} marginLeft={`10px`}>
+              本当に移動しますか？
+            </Heading>
+            <Heading size={`sm`} opacity={`0.5`} margin={`10px`}>
               リンクをコピーせずにホームへ戻るとこのroomへは戻れなくなります
             </Heading>
-          </ModalHeader>
-          <ModalBody></ModalBody>
 
-          <ModalFooter>
-            <Flex gap={`5rem`}>
-              <Button
-                bgGradient={`linear(to-r, ${colors.pink} 2.08%, ${colors.purple} 45.11%, ${colors.blue} 100%)`}
-                mr={3}
-                onClick={onClose}
-                color={colors.white}
-              >
-                Cancel
-              </Button>
-              <Link href={`/`}>
-                <Button variant="outline">move</Button>
-              </Link>
+            <Flex justifyContent="space-between" paddingTop={`10px`}>
+              <Flex>
+                <Button
+                  bgColor={`#84A6EE`}
+                  mr={3}
+                  onClick={onClose}
+                  color={colors.white}
+                  marginLeft={`15px`}
+                >
+                  Cancel
+                </Button>
+              </Flex>
+              <Flex marginRight={`15px`}>
+                <Link href={`/`}>
+                  <Button variant="outline">move</Button>
+                </Link>
+              </Flex>
             </Flex>
-          </ModalFooter>
+          </ModalHeader>
         </ModalContent>
       </Modal>
     </>
