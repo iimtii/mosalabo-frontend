9b2a796dd6a8b8074099bbff927543ae62734315
Tomoyuki Hiranuma <tomoyuki.hiranuma@rakuten.com>
2022-05-24 15:59:09 +0900

    add: display gallary

--- src/components/GallaryDisplay.jsx
+++ src/components/GallaryDisplay.jsx
@@ -1,19 +1,21 @@
 import { useContext } from "react";
 import { GallaryContext } from "../contexts/GallaryContext";
-import Image from "next/image";
-import { Center } from "@chakra-ui/react";
+import { Center, Grid } from "@chakra-ui/react";
+import { Gallary } from "./Gallary";
 
 export const GallaryDisplay = () => {
   const { currentGallary } = useContext(GallaryContext);
+
   if (!currentGallary || currentGallary.length === 0)
     return <Center>No images exist...</Center>;
-  return currentGallary.map((path, index) => (
-    <Image
-      key={index}
-      alt={`img${index}`}
-      src={path}
-      width="200px"
-      height="200px"
-    />
-  ));
+
+  return (
+    <Grid templateColumns={`repeat(3, 1fr)`}>
+      {currentGallary.map((img, index) => (
+        <>
+          <Gallary src={img.path} key={index} />
+        </>
+      ))}
+    </Grid>
+  );
 };
