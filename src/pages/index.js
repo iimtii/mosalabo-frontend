029384f1444b57d236b1de18df45a90199660b4c
imtiar <imtiar.jahan@rakuten.com>
2022-05-24 10:53:41 +0900

    tmp

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,16 +4,6 @@ import {
   Flex,
   Box,
   Button,
-  Image,
-  src,
-  handleSubmit,
-  Modal,
-  ModalHeader,
-  ModalOverlay,
-  ModalCloseButton,
-  ModalBody,
-  ModalFooter,
-  ModalContent,
   useDisclosure,
 } from "@chakra-ui/react";
 import { colors } from "../styles/common";
@@ -23,84 +13,66 @@ import { Layout } from "../components/common/Layout";
 import { Title } from "../components/Title";
 import { Room } from "../components/Room";
 import { TemplateContext } from "../contexts/TemplateContext";
-
-const { isOpen, onOpen, onClose } = useDisclosure();
+import { EnterOriginalModal } from "/Users/imtiar.jahan/Desktop/teamf-frontend/src/components/modals/EnterOriginalModal.jsx";
 
 const Home = () => {
   const { templates, fetchTemplates } = useContext(TemplateContext);
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   useEffect(() => {
     fetchTemplates();
   }, []);
 
   return (
-    <>
-      <Layout>
-        <Title />
-        <Center paddingBottom={`55px`}>
-          <Flex
-            flexDirection={`column`}
-            gap={`22px`}
-            border={`1px`}
-            borderColor={`#E2E8F0`}
-            paddingTop={`30px`}
-            paddingBottom={`16px`}
-            borderRadius={`12px`}
-            height={`180px`}
-            width={`266px`}
-          >
-            <Box m={`auto`}>
-              <Image width={`60px`} height={`60px`} objectFit={`contain`} />
-            </Box>
-            <Box m={`auto`} onClick={onOpen}>
-              <Button
-                m={`auto`}
-                borderRadius={`24px`}
-                width={`172px`}
-                bgColor={colors.primary}
-                color={colors.white}
-              >
-                自分でテーマを作る
-              </Button>
-            </Box>
-          </Flex>
-        </Center>
-
-        <Grid
-          templateColumns={`repeat(2, 1fr)`}
-          width={`68%`}
-          margin={`auto`}
-          gap={`40px`}
-          paddingBottom={`91px`}
+    <Layout>
+      <Title />
+      <Center paddingBottom={`55px`}>
+        <Flex
+          flexDirection={`column`}
+          gap={`22px`}
+          border={`1px`}
+          borderColor={`#E2E8F0`}
+          paddingTop={`30px`}
+          paddingBottom={`16px`}
+          borderRadius={`12px`}
+          height={`180px`}
+          width={`266px`}
         >
-          {templates.map((template) => (
-            <Room
-              key={template.id}
-              id={template.id}
-              src={template.themeImagePath}
-              iconSrc={template.iconImagePath}
-              maximumImage={template.maximumImage}
-            />
-          ))}
-        </Grid>
-      </Layout>
-
-      <Modal isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Modal Title</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody></ModalBody>
-
-          <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={onClose}>
-              Close
+          <Box m={`auto`}></Box>
+          <Box m={`auto`} onClick={onOpen}>
+            <Button
+              m={`auto`}
+              borderRadius={`24px`}
+              width={`172px`}
+              bgColor={colors.primary}
+              color={colors.white}
+            >
+              自分でテーマを作る
             </Button>
-            <Button variant="ghost">Secondary Action</Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
-    </>
+          </Box>
+        </Flex>
+      </Center>
+
+      <Grid
+        templateColumns={`repeat(2, 1fr)`}
+        width={`68%`}
+        margin={`auto`}
+        gap={`40px`}
+        paddingBottom={`91px`}
+      >
+        {templates.map((template) => (
+          <Room
+            key={template.id}
+            id={template.id}
+            src={template.themeImagePath}
+            iconSrc={template.iconImagePath}
+            maximumImage={template.maximumImage}
+          />
+        ))}
+      </Grid>
+
+      <EnterOriginalModal isOpen={isOpen} onClose={onClose} />
+    </Layout>
   );
 };
 
