720a69d0e2658c675ac22ec3a22d29b5b2a4dcae
Tomoyuki Hiranuma <tomoyuki.hiranuma@rakuten.com>
2022-05-24 09:12:11 +0900

    refactor: original modal

---
+++ src/components/OriginalRoom.jsx
@@ -0,0 +1,57 @@
+import { Center, Box, Button, Flex, useDisclosure } from "@chakra-ui/react";
+import Image from "next/image";
+import { colors } from "../styles/common";
+import { OriginalRoomModal } from "./modals/OriginalRoomModal";
+
+export const OriginalRoom = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const handleSubmit = () => {
+    console.log("click");
+  };
+
+  return (
+    <>
+      <Center paddingBottom={`55px`}>
+        <Flex
+          flexDirection={`column`}
+          gap={`22px`}
+          border={`1px`}
+          borderColor={`#E2E8F0`}
+          paddingTop={`30px`}
+          paddingBottom={`16px`}
+          borderRadius={`12px`}
+          height={`180px`}
+          width={`266px`}
+        >
+          <Box m={`auto`}>
+            <Image
+              alt="original"
+              src={`/icons/home.svg`}
+              width={`60px`}
+              height={`60px`}
+              objectFit={`contain`}
+            />
+          </Box>
+          <Box m={`auto`}>
+            <Button
+              onClick={onOpen}
+              borderRadius={`24px`}
+              width={`172px`}
+              bgColor={colors.primary}
+              color={colors.white}
+            >
+              自分でテーマを作る
+            </Button>
+          </Box>
+        </Flex>
+      </Center>
+
+      <OriginalRoomModal
+        isOpen={isOpen}
+        onClose={onClose}
+        handleSubmit={handleSubmit}
+      />
+    </>
+  );
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,11 @@
-import {
-  Grid,
-  Center,
-  Flex,
-  Box,
-  Button,
-  onOpen,
-  Image,
-  isOpen,
-  onClose,
-  src,
-  handleSubmit,
-  Modal,
-  ModalOverlay,
-  ModalCloseButton,
-  ModalBody,
-  ModalFooter,
-  ModalContent,
-} from "@chakra-ui/react";
-import { colors } from "../styles/common";
+import { Grid } from "@chakra-ui/react";
 import { useEffect } from "react";
 import { useContext } from "react";
 import { Layout } from "../components/common/Layout";
 import { Title } from "../components/Title";
 import { Room } from "../components/Room";
 import { TemplateContext } from "../contexts/TemplateContext";
-import { RoomCreateModal } from "../components/modals/RoomCreateModal";
+import { OriginalRoom } from "../components/OriginalRoom";
 
 const Home = () => {
   const { templates, fetchTemplates } = useContext(TemplateContext);
@@ -36,64 +17,7 @@ const Home = () => {
   return (
     <Layout>
       <Title />
-      <Center paddingBottom={`55px`}>
-        <Flex
-          flexDirection={`column`}
-          gap={`22px`}
-          border={`1px`}
-          borderColor={`#E2E8F0`}
-          paddingTop={`30px`}
-          paddingBottom={`16px`}
-          borderRadius={`12px`}
-          height={`180px`}
-          width={`266px`}
-        >
-          <Box m={`auto`}>
-            <Image width={`60px`} height={`60px`} objectFit={`contain`} />
-          </Box>
-          <Box m={`auto`}>
-            <Button
-              onClick={onOpen}
-              borderRadius={`24px`}
-              width={`172px`}
-              bgColor={colors.primary}
-              color={colors.white}
-            >
-              自分でテーマを作る
-            </Button>
-          </Box>
-        </Flex>
-      </Center>
-
-      <Modal size={`xs`} isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalCloseButton />
-          <ModalBody>
-            <Center paddingY={`30px`}>このテーマで作成する？</Center>
-            <Center>
-              <Image
-                alt={`selected`}
-                // layout={`fill`}
-                objectFit={`contain`}
-                width={`270px`}
-                height={`350px`}
-              />
-            </Center>
-          </ModalBody>
-          <ModalFooter>
-            <Box m={`auto`}>
-              <Button
-                onClick={handleSubmit}
-                bgColor={colors.primary}
-                color={colors.white}
-              >
-                作成
-              </Button>
-            </Box>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
+      <OriginalRoom />
 
       <Grid
         templateColumns={`repeat(2, 1fr)`}
