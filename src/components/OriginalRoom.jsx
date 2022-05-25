db681cf94afd03c277dda449a82df05966443088
Kamatsuka, Naoki | Naoki | CTMO <naoki.a.kamatsuka@rakuten.com>
2022-05-25 16:46:55 +0900

    change smileiconPng to SVG

--- src/components/OriginalRoom.jsx
+++ src/components/OriginalRoom.jsx
@@ -23,7 +23,7 @@ export const OriginalRoom = () => {
           <Box m={`auto`}>
             <Image
               alt="original"
-              src={`/icons/smile-icon.png`}
+              src={`/icons/smile-icon.svg`}
               width={`60px`}
               height={`60px`}
               objectFit={`contain`}
