57be257acad8bed02f5f6a3d323bf3bdf446960b
imtiar <imtiar.jahan@rakuten.com>
2022-05-31 11:15:07 +0900

    loading timegit add .

--- src/pages/room/[id].js
+++ src/pages/room/[id].js
@@ -22,6 +22,7 @@ import { TemplateDisplay } from "../../components/TemplateDisplay";
 import { ArtDisplay } from "../../components/ArtDisplay";
 import { NotFoundContext } from "../../contexts/NotFoundContext";
 import { LoadingContext } from "../../contexts/LoadingContext";
+import react from "react";
 
 const Room = () => {
   const { currentRoom, fetchRoom } = useContext(RoomContext);
@@ -44,102 +45,104 @@ const Room = () => {
   if (isNotFound) return <NotFound />;
 
   return (
-    <Layout>
-      <Loading />
-      <Box marginTop={`5px`}>
-        {!!currentRoom && !!currentRoom.mosaicImagePath ? (
-          <ArtDisplay />
-        ) : (
-          <AspectRatio maxW={`400px`} ratio={4 / 3} margin={`auto`}>
-            <Box
-              bgColor={`#6495ED`}
-              color={`white`}
-              fontWeight={`extrabold`}
-              opacity={`0.8`}
-            >
-              写真をアップロードしてね！
-            </Box>
-          </AspectRatio>
-        )}
-      </Box>
-      <Flex
-        maxW={`292.5px`}
-        marginX={`auto`}
-        marginTop={
-          !!currentRoom &&
-          currentRoom.originalFlg &&
-          !!currentRoom.themeImagePath
-            ? 10
-            : 12
-        }
-        marginBottom={`9px`}
-        direction={`column`}
-        gap={
-          !!currentRoom &&
-          currentRoom.originalFlg &&
-          !!currentRoom.themeImagePath
-            ? 4
-            : 7
-        }
-      >
-        <Flex gap={5} justifyContent={`space-around`} marginTop={`2px`}>
-          {!!currentRoom &&
-          currentRoom.originalFlg &&
-          !!currentRoom.themeImagePath ? (
-            <Box>
-              <TemplateDisplay />
-            </Box>
-          ) : null}
-          <Flex direction={`column`} justifyContent={`end`}>
-            {remainNumner <= 0 ? (
-              <Box color={`#8EA6ED`}>アート完成！！</Box>
-            ) : (
-              <Box color={`#8EA6ED`} marginTop={`20px`}>
-                あと{remainNumner}枚！
+    <react.Fragment>
+      <Layout>
+        <Loading />
+        <Box marginTop={`5px`}>
+          {!!currentRoom && !!currentRoom.mosaicImagePath ? (
+            <ArtDisplay />
+          ) : (
+            <AspectRatio maxW={`400px`} ratio={4 / 3} margin={`auto`}>
+              <Box
+                bgColor={`#6495ED`}
+                color={`white`}
+                fontWeight={`extrabold`}
+                opacity={`0.8`}
+              >
+                写真をアップロードしてね！
               </Box>
-            )}
+            </AspectRatio>
+          )}
+        </Box>
+        <Flex
+          maxW={`292.5px`}
+          marginX={`auto`}
+          marginTop={
+            !!currentRoom &&
+            currentRoom.originalFlg &&
+            !!currentRoom.themeImagePath
+              ? 10
+              : 12
+          }
+          marginBottom={`9px`}
+          direction={`column`}
+          gap={
+            !!currentRoom &&
+            currentRoom.originalFlg &&
+            !!currentRoom.themeImagePath
+              ? 4
+              : 7
+          }
+        >
+          <Flex gap={5} justifyContent={`space-around`} marginTop={`2px`}>
+            {!!currentRoom &&
+            currentRoom.originalFlg &&
+            !!currentRoom.themeImagePath ? (
+              <Box>
+                <TemplateDisplay />
+              </Box>
+            ) : null}
+            <Flex direction={`column`} justifyContent={`end`}>
+              {remainNumner <= 0 ? (
+                <Box color={`#8EA6ED`}>アート完成！！</Box>
+              ) : (
+                <Box color={`#8EA6ED`} marginTop={`20px`}>
+                  あと{remainNumner}枚！
+                </Box>
+              )}
+            </Flex>
+          </Flex>
+
+          {/* Todo: animation from prev state to next state */}
+          {/* ref: https://github.com/chakra-ui/chakra-ui/issues/68 */}
+          <Box marginY={`20px`}>
+            <Progress
+              as={motion.div}
+              height={`8px`}
+              width={`292.5px`}
+              borderRadius={`10px`}
+              value={progressValue}
+              isAnimated={true}
+              hasStripe={hasStripe}
+              isIndeterminate={isIndeterminate}
+            />
+          </Box>
+          <Flex justifyContent={"center"} maxW={`292.5px`} gap={6}>
+            <Box>
+              <Button
+                paddingY={`30px`}
+                borderRadius={`16px`}
+                color={colors.white}
+                bgGradient={`linear(to-r, ${colors.pink} 2.08%, ${colors.purple} 45.11%, ${colors.blue} 100%)`}
+                onClick={onOpen}
+                height={`90px`}
+                width={`137px`}
+                fontSize={`19px`}
+              >
+                写真を
+                <br />
+                アップロード
+              </Button>
+            </Box>
+            <Box>
+              <ShareButton />
+            </Box>
           </Flex>
         </Flex>
-
-        {/* Todo: animation from prev state to next state */}
-        {/* ref: https://github.com/chakra-ui/chakra-ui/issues/68 */}
-        <Box marginY={`20px`}>
-          <Progress
-            as={motion.div}
-            height={`8px`}
-            width={`292.5px`}
-            borderRadius={`10px`}
-            value={progressValue}
-            isAnimated={true}
-            hasStripe={hasStripe}
-            isIndeterminate={isIndeterminate}
-          />
-        </Box>
-        <Flex justifyContent={"center"} maxW={`292.5px`} gap={6}>
-          <Box>
-            <Button
-              paddingY={`30px`}
-              borderRadius={`16px`}
-              color={colors.white}
-              bgGradient={`linear(to-r, ${colors.pink} 2.08%, ${colors.purple} 45.11%, ${colors.blue} 100%)`}
-              onClick={onOpen}
-              height={`90px`}
-              width={`137px`}
-              fontSize={`19px`}
-            >
-              写真を
-              <br />
-              アップロード
-            </Button>
-          </Box>
-          <Box>
-            <ShareButton />
-          </Box>
-        </Flex>
-      </Flex>
-      <Footer />
-      <UploadModal isOpen={isOpen} onClose={onClose} />
-    </Layout>
+        <Footer />
+        <UploadModal isOpen={isOpen} onClose={onClose} />
+      </Layout>
+    </react.Fragment>
   );
 };
 
