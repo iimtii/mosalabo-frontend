b6278f453e228713d6f4a2f5b9925cd90ff1caed
Tomoyuki Hiranuma <tomoyuki.hiranuma@rakuten.com>
2022-06-01 14:53:18 +0900

    fix: https

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-axios.defaults.baseURL = process.env.NEXT_PUBLIC_API_URL;
+axios.defaults.baseURL = process.env.NEXT_PUBLIC_API_HTTP_URL;
+if (process.browser && location.protocol === "https:")
+  axios.defaults.baseURL = process.env.NEXT_PUBLIC_API_HTTPS_URL;
 // axios.defaults.baseURL = "http://localhost:3000/api/";
 axios.defaults.headers.post["Content-Type"] = "application/json";
 axios.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
