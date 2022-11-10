d1f2a96abb478b763ad8cac52c9d941f8d9bb825
tomoyuki-hiranuma <numanumatom@gmail.com>
2022-11-10 22:57:55 +0900

    fix

--- src/constants/common/index.js
+++ src/constants/common/index.js
@@ -1,4 +1,5 @@
 export const APP_NAME = "Mosalabo";
+
 export const APP_DESCRIPTION = "create mosaic arts with coorporation";
 
 export const INSTAGRAM_URL = "https://www.instagram.com/";
--- src/constants/room/index.js
+++ src/constants/room/index.js
@@ -1,11 +1,15 @@
 export const CLIPPED_SUCCESS = "share link clipped!";
 
 export const MAX_NUMBER_OF_IMAGES = 15;
+
 export const MAX_NUMBER_OF_SELECTED_IMAGE = 20;
+
 export const MAX_IMAGE_BYTES = 5 * 10 ** 6; // 5MB
 
 export const MAX_WIDTH = 300;
+
 export const MAX_HEIGHT = 300;
 
 export const MAX_ORIGINAL_WIDTH = 500;
+
 export const MAX_ORIGINAL_HEIGHT = 500;
