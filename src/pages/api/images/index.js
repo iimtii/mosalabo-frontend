5a782671fdeb22ceef0382033fa2228240487b29
tomoyuki-hiranuma <numanumatom@gmail.com>
2022-11-23 18:36:53 +0900

    chore: post images with mock api

--- src/pages/api/images/index.js
+++ src/pages/api/images/index.js
@@ -2,12 +2,11 @@ export default function handler(req, res) {
   if (req.method === "POST") {
     res.status(200).json({
       uuid: "550e8400-e29b-41d4-a716-446655440000",
-      themeImagePath:
-        "https://mosalabo-images.s3.ap-northeast-1.amazonaws.com/Cat.jpg",
-      numberOfImage: 122,
-      maximumImage: 250,
-      mosaicImagePath:
-        "https://mosalabo-images.s3.ap-northeast-1.amazonaws.com/Cat.jpg",
+      themeImagePath: "/sample/images/tiger.jpg",
+      numberOfImage: 22,
+      maximumImage: 100,
+      mosaicImagePath: "/sample/images/tiger.jpg",
+      originalFlg: true,
       createdAt: "2022-05-12T12:32:36Z",
     });
   }
