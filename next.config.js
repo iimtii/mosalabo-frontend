6cdf0ed27279f8b80946fb4494935e8f956f9c08
tomoyuki-hiranuma <numanumatom@gmail.com>
2022-12-29 19:51:21 +0900

    fix: for prod

--- next.config.js
+++ next.config.js
@@ -1,18 +1,20 @@
 /** @type {import('next').NextConfig} */
 const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
 
 const nextConfig = {
-  // pwa: {
-  //   dest: "public",
-  //   register: true,
-  //   skipWaiting: true,
-  //   disable: process.env.NODE_ENV === "development",
-  //   buildExcludes: [/.*\.js\.map/],
-  // },
-  // images: {
-  //   domains: ["mosalabo-images.s3.ap-northeast-1.amazonaws.com"],
-  // },
   reactStrictMode: true,
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    runtimeCaching,
+    disable: process.env.NODE_ENV === "development",
+    buildExcludes: [/.*\.js\.map/],
+  },
+  images: {
+    domains: ["mosalabo-images.s3.ap-northeast-1.amazonaws.com"],
+  },
 };
 
 module.exports =
